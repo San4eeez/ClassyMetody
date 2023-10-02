e22898902bb14cd48cd5524600e397fa5db48e1c
Bogdan <Bogdan@DESKTOP-T7BV8B5>
2023-10-02 14:30:42 +0300

    Добавьте файлы проекта.

---
+++ ClassyMetody/CarAuto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyMetody
+{
+    internal class CarAuto
+    {
+        private string number;
+        private float fuel;
+        private float flow;
+
+        public void info(string number, float fuel, float flow)
+        {
+            this.number = number;
+            this.fuel = fuel;
+            this.flow = flow;
+        }
+        public void outinfo()
+        {
+            Console.WriteLine($"Номер: {this.number}\n Топливо: {this.fuel}\n Расход: {this.flow}");
+        }
+
+        public void zaprawka(float top)
+        {
+            this.fuel += top;
+        }
+        public void move(int km)
+        {
+            
+            Console.WriteLine($"осталось после поездки {this.fuel-(km*(this.flow)/100)} литров");
+        }
+    }
+}
---
+++ ClassyMetody/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClassyMetody;
+
+CarAuto carAuto = new CarAuto();
+carAuto.info("п248он", 40, 4.0F);
+carAuto.outinfo();
+Console.WriteLine("Шеф, сколько льём?: ");
+carAuto.zaprawka(float.Parse(Console.ReadLine()));
+Console.WriteLine("Вводи сколько км: ");
+carAuto.move(km: int.Parse(Console.ReadLine()));
